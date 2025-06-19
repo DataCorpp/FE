9c5805d4208443f849aaf37f4d531b1e798a7f4c
Trungnef <cauchubebong184@gmail.com>
2025-06-19 11:08:20 +0700

    .

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -207,7 +207,7 @@ const SignInForm = () => {
   const handleGoogleSignIn = () => {
     // Mở popup để đăng nhập Google
     const googleOAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+    const clientId = import.meta.env.GOOGLE_CLIENT_ID;
     
     // Biết chính xác redirect URI đã được đăng ký trong Google Console
     const redirectUri = 'http://localhost:8080/google-auth-callback.html';
